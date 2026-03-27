671311c52dc452a03af5b2ee853d690436309579
Vinicius Macedo <macedovinicius17@gmail.com>
2026-03-27 18:04:24 -0300

    funcinoalidade adicionada - falta adicinar o checklist

--- script.js
+++ script.js
@@ -2,25 +2,34 @@ const listaDeTarefasAdicionadas = [];
 
 const adicionarTarefa = () => {
     const input =  document.querySelector("#input-texto-da-tarefa").value;
-    listaDeTarefasAdicionadas.push(input);
 
-    const textoNenhumaTarefaAdicionada = document.querySelector("#nenhuma-tarefa-adicionada-texto");
-    textoNenhumaTarefaAdicionada.classList.add("none");
+    if (input === "" || input ===  " " || input === null) {
+        alert("Por favor, adicione ao menos uma tarefa!");
+    } else {
+        document.querySelector(".div-container-contendo-as-tarefas").style.justifyContent = "flex-start";
 
-    const indicadorTarefasEmTempoReal = document.querySelector("span");
-    indicadorTarefasEmTempoReal.innerHTML = listaDeTarefasAdicionadas.length;
-    
-    const divContendoAsTarefas = document.querySelector(".div-container-contendo-as-tarefas");
-    divContendoAsTarefas.innerHTML = "";
-    
-    listaDeTarefasAdicionadas.forEach((el) => {
+        listaDeTarefasAdicionadas.push(input);
+
+        const indicadorTarefasEmTempoReal = document.querySelector(".numero-de-tarefas-em-tempo-real");
+        indicadorTarefasEmTempoReal.innerHTML = listaDeTarefasAdicionadas.length;
         
+        const divContendoAsTarefas = document.querySelector(".div-container-contendo-as-tarefas");
+        divContendoAsTarefas.innerHTML = "";
+        
+        listaDeTarefasAdicionadas.forEach((el) => {
+            const tarefaCriada = document.createElement("li");
+            tarefaCriada.classList.add("tarefa-criada");
+            tarefaCriada.textContent = el;
 
-        const tarefaCriada = document.createElement("li");
-        tarefaCriada.classList.add("tarefa-criada");
-        tarefaCriada.innerHTML = el;
+            divContendoAsTarefas.appendChild(tarefaCriada);
+        });
 
-        divContendoAsTarefas.appendChild(tarefaCriada);
-
-    });
+        document.querySelector("#input-texto-da-tarefa").value = "";
+        }
 }
+
+const textoLimparConcluidas = document.querySelector("#limpar-concluidas-texto");
+
+textoLimparConcluidas.addEventListener("click", () => {
+
+});
