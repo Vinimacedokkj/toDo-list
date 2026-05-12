99da7aa02b474921dc05008a94530e4059377977
vinicius.macedo <macedovinicius17@gmail.com>
2026-05-12 16:36:04 -0300

    simpifiquei a função, tirei o código duplicado

--- script.js
+++ script.js
@@ -21,138 +21,72 @@ const adicionarTarefa = () => {
         
         const divContendoAsTarefas = document.querySelector(".div-container-contendo-as-tarefas");
         
-        const renderizarLista = (existeTarefaConcluida) => {
-            if (!existeTarefaConcluida) {      
-                console.log("Não existem tarefas finalizadas");
-                
-                divContendoAsTarefas.innerHTML = "";
+        const renderizarLista = () => {
+            divContendoAsTarefas.innerHTML = "";
 
-                listaDeTarefasAdicionadas.forEach(tarefa => {
-                    const divDeCadaTarefa = document.createElement("div");
-                    divDeCadaTarefa.classList.add("tarefa-criada");
-        
-                    const checklistDaTarefa = document.createElement("input");
-                    checklistDaTarefa.type = "checkbox";
-        
-                    const textoDaTarefa = document.createElement("p");
-                    textoDaTarefa.textContent = tarefa.texto;
+            listaDeTarefasAdicionadas.forEach(tarefa => {
+                const divDeCadaTarefa = document.createElement("div");
+                divDeCadaTarefa.classList.add("tarefa-criada");
+    
+                const checklistDaTarefa = document.createElement("input");
+                checklistDaTarefa.type = "checkbox";
+    
+                const textoDaTarefa = document.createElement("p");
+                textoDaTarefa.textContent = tarefa.texto;
 
-                    const concluirTarefa = () => {
-                        if (checklistDaTarefa.checked) {
-                            tarefa.concluida = true;
+                const concluirTarefa = () => {
+                    if (checklistDaTarefa.checked) {
+                        tarefa.concluida = true;
 
-                            textoDaTarefa.style.textDecoration = "line-through";
-                            textoDaTarefa.style.color = "var(--cor-cinza-soft)";
-                        } else {
-                            tarefa.concluida = false;
-
-                            textoDaTarefa.style.textDecoration = "none";
-                            textoDaTarefa.style.color = "black";
-                        }
-                    }
-                    
-                    checklistDaTarefa.addEventListener("change", concluirTarefa);
-        
-                    const botaoExcluirTarefa = document.createElement("p");
-                    botaoExcluirTarefa.classList.add("excluir-tarefa");
-                    botaoExcluirTarefa.textContent = "x";
-        
-                    divContendoAsTarefas.appendChild(divDeCadaTarefa);
-        
-                    divDeCadaTarefa.appendChild(checklistDaTarefa);
-                    divDeCadaTarefa.appendChild(textoDaTarefa);
-                    divDeCadaTarefa.appendChild(botaoExcluirTarefa);
-
-                    const excluirTarefa = () => {
-                        const idDoBotao = tarefa.id;
-                        
-                        listaDeTarefasAdicionadas = listaDeTarefasAdicionadas.filter(element => element.id !== idDoBotao);
-
-                        listaDeTarefasAdicionadas.forEach(obj => renderizarLista());
-
-                        indicadorTarefasEmTempoReal.innerHTML = listaDeTarefasAdicionadas.length;
-
-                        if (listaDeTarefasAdicionadas.length === 0) {
-                            divContendoAsTarefas.style.justifyContent = "center";
-                            divContendoAsTarefas.style.color = "var(--cor-cinza)";
-                            divContendoAsTarefas.innerHTML = "Nenhuma tarefa adicionada. Adicione a primeira!";
-                        }
-                    }
-
-                    botaoExcluirTarefa.addEventListener("click", excluirTarefa);
-                });
-            } else {
-                console.log("Existem tarefas finalizadas");
-
-                divContendoAsTarefas.innerHTML = "";
-
-                console.log(listaDeTarefasAdicionadas);
-
-                listaDeTarefasAdicionadas.forEach(tarefa => {
-                    const divDeCadaTarefa = document.createElement("div");
-                    divDeCadaTarefa.classList.add("tarefa-criada");
-        
-                    const checklistDaTarefa = document.createElement("input");
-                    checklistDaTarefa.type = "checkbox";
-        
-                    const textoDaTarefa = document.createElement("p");
-                    textoDaTarefa.textContent = tarefa.texto;
-
-                    const concluirTarefa = () => {
-                        if (checklistDaTarefa.checked) {
-                            tarefa.concluida = true;
-
-                            textoDaTarefa.style.textDecoration = "line-through";
-                            textoDaTarefa.style.color = "var(--cor-cinza-soft)";
-                        } else {
-                            tarefa.concluida = false;
-
-                            textoDaTarefa.style.textDecoration = "none";
-                            textoDaTarefa.style.color = "black";
-                        }
-                    }
-                    
-                    checklistDaTarefa.addEventListener("change", concluirTarefa);
-        
-                    const botaoExcluirTarefa = document.createElement("p");
-                    botaoExcluirTarefa.classList.add("excluir-tarefa");
-                    botaoExcluirTarefa.textContent = "x";
-        
-                    divContendoAsTarefas.appendChild(divDeCadaTarefa);
-        
-                    divDeCadaTarefa.appendChild(checklistDaTarefa);
-                    divDeCadaTarefa.appendChild(textoDaTarefa);
-                    divDeCadaTarefa.appendChild(botaoExcluirTarefa);
-
-                    const excluirTarefa = () => {
-                        const idDoBotao = tarefa.id;
-                        
-                        listaDeTarefasAdicionadas = listaDeTarefasAdicionadas.filter(element => element.id !== idDoBotao);
-
-                        listaDeTarefasAdicionadas.forEach(obj => renderizarLista());
-
-                        indicadorTarefasEmTempoReal.innerHTML = listaDeTarefasAdicionadas.length;
-
-                        if (listaDeTarefasAdicionadas.length === 0) {
-                            divContendoAsTarefas.style.justifyContent = "center";
-                            divContendoAsTarefas.style.color = "var(--cor-cinza)";
-                            divContendoAsTarefas.innerHTML = "Nenhuma tarefa adicionada. Adicione a primeira!";
-                        }
-                    }
-
-                    botaoExcluirTarefa.addEventListener("click", excluirTarefa);
-
-                    if (tarefa.concluida === true) {
-                        checklistDaTarefa.checked = "true";
                         textoDaTarefa.style.textDecoration = "line-through";
                         textoDaTarefa.style.color = "var(--cor-cinza-soft)";
-                    } 
-                });
-            } 
+                    } else {
+                        tarefa.concluida = false;
+
+                        textoDaTarefa.style.textDecoration = "none";
+                        textoDaTarefa.style.color = "black";
+                    }
+                }
+                
+                checklistDaTarefa.addEventListener("change", concluirTarefa);
+    
+                const botaoExcluirTarefa = document.createElement("p");
+                botaoExcluirTarefa.classList.add("excluir-tarefa");
+                botaoExcluirTarefa.textContent = "x";
+    
+                divContendoAsTarefas.appendChild(divDeCadaTarefa);
+    
+                divDeCadaTarefa.appendChild(checklistDaTarefa);
+                divDeCadaTarefa.appendChild(textoDaTarefa);
+                divDeCadaTarefa.appendChild(botaoExcluirTarefa);
+
+                const excluirTarefa = () => {
+                    const idDoBotao = tarefa.id;
+                    
+                    listaDeTarefasAdicionadas = listaDeTarefasAdicionadas.filter(element => element.id !== idDoBotao);
+
+                    listaDeTarefasAdicionadas.forEach(obj => renderizarLista());
+
+                    indicadorTarefasEmTempoReal.innerHTML = listaDeTarefasAdicionadas.length;
+
+                    if (listaDeTarefasAdicionadas.length === 0) {
+                        divContendoAsTarefas.style.justifyContent = "center";
+                        divContendoAsTarefas.style.color = "var(--cor-cinza)";
+                        divContendoAsTarefas.innerHTML = "Nenhuma tarefa adicionada. Adicione a primeira!";
+                    }
+                }
+
+                botaoExcluirTarefa.addEventListener("click", excluirTarefa);
+
+                if (tarefa.concluida === true) {
+                    checklistDaTarefa.checked = "true";
+                    textoDaTarefa.style.textDecoration = "line-through";
+                    textoDaTarefa.style.color = "var(--cor-cinza-soft)";
+                } 
+            });
         }
 
-        const existeTarefaConcluida = listaDeTarefasAdicionadas.some(tarefa => tarefa.concluida === true);
-        renderizarLista(existeTarefaConcluida);
+        renderizarLista();
 
         document.querySelector("#input-texto-da-tarefa").value = "";
     };
